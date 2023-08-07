32f3d55c3afcac757ae0767f620cefa0c9dab602
Rimpi <rimpiranibaruah123@gmail.com>
2023-08-07 21:34:38 +0530

    minor change

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const path = require('path');
-const http = require('http');
-const express = require('express');
+const path = require("path");
+const http = require("http");
+const express = require("express");
 const socket = require("socket.io");
 const bodyParser = require("body-parser");
 const Filter = require("bad-words");
@@ -11,10 +11,10 @@ const {
   userJoin,
   getCurrentUser,
   userLeave,
-  getRoomUsers
+  getRoomUsers,
 } = require("./utils/users");
 
-app.use(require('cors')());
+app.use(require("cors")());
 
 const server = http.createServer(app);
 
@@ -32,17 +32,10 @@ app.get("/chat", (req, res) => {
   res.sendFile(__dirname + "/public/chat.html");
 });
 
-
-
 const botName = "🤖Bot";
 
-io.on('connection', socket => {
-
-  socket.on("joinRoom", ({
-    username,
-    room
-  }) => {
-
+io.on("connection", (socket) => {
+  socket.on("joinRoom", ({ username, room }) => {
     //Add the user to the room
     const user = userJoin(socket.id, username, room);
 
@@ -52,54 +45,64 @@ io.on('connection', socket => {
     socket.emit("message", formatMessage(botName, "Welcome to ChatRoom"));
 
     //Broadcast when a user connects
-    socket.broadcast.to(user.room).emit("message", formatMessage(botName, `${user.username} has joined the room`));
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(botName, `${user.username} has joined the room`)
+      );
 
     //Send users and room info
     io.to(user.room).emit("roomUsers", {
       room: user.room,
-      users: getRoomUsers(user.room)
+      users: getRoomUsers(user.room),
     });
-
   });
-
 
   //Listen for chatMessage
   socket.on("chatMessage", (inputMessage) => {
     const user = getCurrentUser(socket.id);
 
     const filter = new Filter();
-    if(filter.isProfane(inputMessage)){
-      socket.emit("message", formatMessage(botName, "Profanity is not allowed"));
+    if (filter.isProfane(inputMessage)) {
+      socket.emit(
+        "message",
+        formatMessage(botName, "Profanity is not allowed")
+      );
+    } else {
+      io.to(user.room).emit(
+        "message",
+        formatMessage(user.username, inputMessage)
+      );
     }
-    else{
-      io.to(user.room).emit("message", formatMessage(user.username, inputMessage));
-    }
-  })
+  });
 
   // Listen for typing
-  socket.on("typing", username => {
+  socket.on("typing", (username) => {
     const user = getCurrentUser(socket.id);
     socket.broadcast.to(user.room).emit("typing", username);
-  })
+  });
 
-  socket.on("disconnect", function() {
+  socket.on("disconnect", function () {
     const user = userLeave(socket.id);
 
-    if(user){
-       io.to(user.room).emit("message", formatMessage(botName, `${user.username} has left the room`));
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        formatMessage(botName, `${user.username} has left the room`)
+      );
 
-       //Send users and room info
-       io.to(user.room).emit("roomUsers", {
-         room: user.room,
-         users: getRoomUsers(user.room)
-       });
+      //Send users and room info
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
     }
-
   });
 });
 
 const port = process.env.PORT || 3000;
-
+//Listening to server
 server.listen(port, () => {
-  console.log("Server started at " + (port));
+  console.log("Server started at " + port);
 });
